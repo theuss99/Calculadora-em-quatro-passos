4fc96f52824aba110ef8f147a08f3fad34ee2c7c
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 19:50:34 -0300

    Calcula a função soma

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+print("                         Calculadora")
+
+
+n1=float(input("Digite o primeiro valor: "))
+n2=float(input("Digite o segundo valor: "))
+soma=n1+n2
+print("O resultado é:",soma)
